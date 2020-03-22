1bfe4b01418d44fdb80638c981d494b7786d5154
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-03-22 19:57:09 +0100

    Template reference and ViewChild

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,7 +8,7 @@ import { Quotation } from '../models/quotation';
 })
 export class FormComponent {
   @Output() newQuotation = new EventEmitter<Quotation>();
-  showForm = true;
+  showForm = false;
   quotation: Quotation = { author: '', sentence: '', votes: 0 };
 
   onSwitchForm(): void {
