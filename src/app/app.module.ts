afdf938b09590ec38f827b4d9fdc4bef69a868fd
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-03-24 15:35:34 +0100

    Ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FormComponent } from './form/form.component';
     NavbarComponent,
     RankingComponent,
     FormComponent,
+    ButtonComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
